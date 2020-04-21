1808abf9d5b06746780d30dec5d8c6ca63c4bf3a
Dan00n <60699328+Dan00n@users.noreply.github.com>
2020-04-21 18:46:00 +0200

    3

--- csvtest.cs
+++ csvtest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
+//using System.Collections.Generic;
+//using System.IO;
+//using Newtonsoft.Json.Linq;
 namespace projectb
 {
     public class csvtest
@@ -9,28 +10,41 @@ namespace projectb
 
         public static void CSVTEST()
         {
-            using(var reader = new StreamReader(@"grades.csv"))
-            {
-                List<String> ListA = new List<string>();
-                List<String> ListB = new List<string>();
-                while (!reader.EndOfStream)
-                {
-                    var line = reader.ReadLine();
-                    var values = line.Split(',');
 
-                    ListA.Add(values[0]);
-                    ListB.Add(values[1]);
-                }
-                Console.WriteLine("grades.csv");
-                File.WriteAllText("grades.csv", csv);
+           // JObject parsed = JObject.Parse("menulists.json");
+           // foreach(var pair in parsed)
+           // {
+           //     Console.WriteLine("{0}: {1}", pair.Key, pair.Value);
+           // }
+
+
+
+
+
+
+
+           // using(var reader = new StreamReader(@"grades.csv"))
+           // {
+           //     List<String> ListA = new List<string>();
+           //     List<String> ListB = new List<string>();
+           //     while (!reader.EndOfStream)
+           //     {
+           //         var line = reader.ReadLine();
+           //         var values = line.Split(',');
+
+            //        ListA.Add(values[0]);
+            //        ListB.Add(values[1]);
+            //    }
+            //    Console.WriteLine("grades.csv");
+             //   File.WriteAllText("grades.csv", csv);
                 
-            }
+          //  }
            
 
 
             //File.ReadAllText(@"\grades.csv");
             //addR("dae","egaeg","2342","grades.csv");
-        }
+      //  }
        // public static void addR(string last, string first, string num, string filepath)
       //  {
      //       try
@@ -43,6 +57,7 @@ namespace projectb
          //   {
         //        throw new ApplicationException("gekke error dit", ex);
         //    }
-        //}
+
+        }
     }
 }
