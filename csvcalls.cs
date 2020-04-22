628e2082ed2a236e6bcec8712a4e058dbc405db7
Dan00n <60699328+Dan00n@users.noreply.github.com>
2020-04-22 12:59:11 +0200

    Big changes
    Updates aan het menu.cs files omdat er een foutje zat. Ook begin van veranderen van het menu.

---
+++ csvcalls.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+
+namespace projectb
+{
+    public class csvcalls
+    {
+        static bool changeRun = true;
+        public static void DRINKS()
+        {
+            string pathstring = "/Users/danine/Documents/School/Programmas/GitHub/QJ7VEG9YggCYmzK5/Drinks.csv";
+            var csvread = File.ReadAllText(pathstring);
+            String[] file = csvread.Split(new char[] { '"', ';' }, StringSplitOptions.RemoveEmptyEntries);
+         
+            foreach (var i in file)
+            {
+               Console.Write(i);
+            }
+        }
+        public static void FOOD()
+        {
+            string pathstring = "/Users/danine/Documents/School/Programmas/GitHub/QJ7VEG9YggCYmzK5/Food.csv";
+            var csvread = File.ReadAllText(pathstring);
+            String[] file = csvread.Split(new char[] { '"', ';' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var i in file)
+            {
+                Console.Write(i);
+            }
+        }
+        public static void SPECIALS()
+        {
+            string pathstring = "/Users/danine/Documents/School/Programmas/GitHub/QJ7VEG9YggCYmzK5/Specials.csv";
+            var csvread = File.ReadAllText(pathstring);
+            String[] file = csvread.Split(new char[] { '"', ';' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var i in file)
+            {
+                Console.Write(i);
+            }
+        }
+        public static void CHANGEask()
+        {
+            Console.WriteLine("Which file would you like to read and change?\n" +
+                "Choose from: Drinks, Food, Specials or Daily");
+            string changeIN = Console.ReadLine();
+            while (changeRun)
+            {
+                if (changeIN.Equals("Drinks", StringComparison.OrdinalIgnoreCase))
+                {
+                    changeRun = false;
+                    DRINKS();
+                    //CHANGEcsv();
+                }
+                else if (changeIN.Equals("Food", StringComparison.OrdinalIgnoreCase))
+                {
+                    changeRun = false;
+                }
+                else if (changeIN.Equals("Specials", StringComparison.OrdinalIgnoreCase))
+                {
+                    changeRun = false;
+                }
+                else if (changeIN.Equals("Daily", StringComparison.OrdinalIgnoreCase))
+                {
+                    changeRun = false;
+                }
+                else
+                {
+                    Console.WriteLine("Not a valid input, please try again.");
+                }
+            }
+
+        }
+
+
+        public static void CHANGEcsv()
+        {
+            // Chilkat.Csv csv = new Chilkat.Csv();
+
+            //  Prior to loading the CSV file, indicate that the 1st row
+            //  should be treated as column names:
+            // csv.HasColumnNames = true;
+
+            //  Load the CSV records from the file:
+            //  success = csv.LoadFile("sample.csv");
+
+
+            //  Change "cheese" to "baguette"
+            //  "cheese" is at row=0, column=3
+            //success = csv.SetCell(0, 3, "baguette");
+
+            //  Write the updated CSV to a string and display:
+            //     string csvDoc;
+            //     csvDoc = csv.SaveToString();
+            //    Console.WriteLine(csvDoc);
+
+            //  Save the CSV to a file:
+            //   success = csv.SaveFile("out.csv");
+            //hier dan dezelfde weer als savefile?
+        }
+
+    }
+}
--- menu.cs
+++ menu.cs
@@ -7,150 +7,87 @@ namespace testproject1
     
     public class Menu
     {
+        static bool chosen = true;
         public static void menu()
         {
             // day today
             var date = DateTime.Now;
             string a = date.ToString("dddd");
+            Console.WriteLine("-------------------------------\n|     Welcome to the menu     |\n-------------------------------");
+            Console.WriteLine(" [DO] - Daily offers \n [S] - Special offers\n [F] - Food\n [D] - Drink");
 
-            Menu();
-            void Menu()
+            while (chosen)
             {
-
-
-
-                Console.WriteLine("-------------------------------\n|     Welcome to the menu     |\n-------------------------------");
-
+                Console.Write("Choose an option: ");
+                string choiceM = Console.ReadLine();
+                if (choiceM.Equals("do", StringComparison.OrdinalIgnoreCase))
                 {
-                    Console.WriteLine(" [A] - Daily offers \n [B] - Special offers\n [F] - Food\n [D] - Drink");
-
-                    Console.Write("Choose an option: ");
-                    string choiceM = Console.ReadLine();
-
+                    chosen = false;
+                    DailyOffer();
+                    void DailyOffer() //Daily offers
                     {
-
-
-                        if (choiceM.Equals("A", StringComparison.OrdinalIgnoreCase))
-                        {
-                            DailyOffer();
-
-                            void DailyOffer() //Daily offers
-                            {
-
-
-                                Console.WriteLine("*** Welcome to the Daily Offers ***");
-
-                                // date today
-                                Console.WriteLine("     " + date.ToLongDateString());
-
-                                if (a.Equals("Monday", StringComparison.OrdinalIgnoreCase))
-                                {
-                                    Console.WriteLine("\n*** The daily offer of today is  ***\n");
-                                    Console.WriteLine("[38] Pizza Salame + Salade for only €12 ");
-                                }
-                                else if (a.Equals("Tuesday", StringComparison.OrdinalIgnoreCase))
-                                {
-                                    Console.WriteLine("\n*** The daily offer of today is  ***\n");
-                                    Console.WriteLine("[39] Filetto al Griglia + Smirnoff Vodka for only €35 ");
-                                }
-                                else if (a.Equals("Wednesday", StringComparison.OrdinalIgnoreCase))
-                                {
-                                    Console.WriteLine("\n*** The daily offer of today is  ***\n");
-                                    Console.WriteLine("[40] Filetto al Funghi + Jack Daniel Whiskey for only €30 ");
-                                }
-                                else if (a.Equals("Thursday", StringComparison.OrdinalIgnoreCase))
-                                {
-                                    Console.WriteLine("\n*** The daily offer of today is  ***\n");
-                                    Console.WriteLine("[41] Filetto al Pepe + Salade feta for only €15 ");
-                                }
-                                else if (a.Equals("Friday", StringComparison.OrdinalIgnoreCase))
-                                {
-                                    Console.WriteLine("\n*** The daily offer of today is  ***\n");
-                                    Console.WriteLine("[42] Scaloppina romana + Moet & Chardon for only €50 ");
-                                }
-                                else if (a.Equals("Saturday", StringComparison.OrdinalIgnoreCase))
-                                {
-                                    Console.WriteLine("\n*** The daily offer of today is  ***\n");
-                                    Console.WriteLine("[43] Pasta Carbonara + Heineken for only €10 ");
-
-                                }
-                                else if (a.Equals("Sunday", StringComparison.OrdinalIgnoreCase))
-                                {
-                                    Console.WriteLine("\n*** The daily offer of today is  ***\n");
-                                    Console.WriteLine("[44] 2 Pizza Pollo + 2 Soft Drinks for only €20 ");
-
-
-                                }
-
-                            }
-
-                        }
-                        else if (choiceM.Equals("B", StringComparison.OrdinalIgnoreCase))
-
-
-                        {
-                            SpecialOffer();
-
-                            void SpecialOffer() //Special offers
-                            {
-
-
-                                Console.WriteLine("*** Welcome to the Special Offers ***************");
-                                Console.WriteLine("[32]------------Pizza + Soft Drink------------€10");
-                                Console.WriteLine("[33]------------Pasta + Soft Drink------------€9");
-                                Console.WriteLine("[34]------------Pizza + Salade----------------€12");
-                                Console.WriteLine("[35]------------Pasta + Salade----------------€10");
-                                Console.WriteLine("[36]------------Meat + Salade-----------------€13");
-                                Console.WriteLine("[37]------------Meat + Soft Drink-------------€11");
-                                Console.WriteLine("*************************************************");
-
-                            }
-
-                        }
-                        else if (choiceM.Equals("f", StringComparison.OrdinalIgnoreCase) || choiceM.Equals("d", StringComparison.OrdinalIgnoreCase))
-                        {
-                            Console.WriteLine("menu f en d------");
-                            csvtest.CSVTEST();
-                            Console.WriteLine("einde menu f en d ------");
-                            
-                            
-
-                        }
-                        
-                        else if (string.IsNullOrEmpty(choiceM))
-                        {   //EMPTY INPUT
-                            Console.WriteLine("Not a valid input, please try again.");
-                        }
-                        else
-                        {   //INVALID INPUT
-                            Console.WriteLine("Not a valid input, please try again.");
-                        }
-
-                        backgo();
-
-                        void backgo()
-                        {
-
-
-                            Console.Write("\nDo you want to go back?[yes/no]");
-                            string goback = Console.ReadLine();
-
-                            if (goback.Equals("yes", StringComparison.OrdinalIgnoreCase))
-                            {
-                                Menu();
-                            }
-                            else if (goback.Equals("no", StringComparison.OrdinalIgnoreCase))
-                            {
-                                Console.Write("\nWhat do you want to do?");
-                            }
-                            else
-                            {
-                                Console.WriteLine("Not a valid input, please try again.");
-                                backgo();
-                            }
-                        }
+                        Console.WriteLine("*** Welcome to the Daily Offers ***");
+                        // date today
+                        Console.WriteLine("     " + date.ToLongDateString());
                     }
                 }
+                else if (choiceM.Equals("s", StringComparison.OrdinalIgnoreCase))
+                {
+                    chosen = false;
+                    Console.WriteLine("*** Welcome to the Special Offers ***************");
+                    csvcalls.SPECIALS(); //get to the csv file specials
+
+                }
+                else if (choiceM.Equals("f", StringComparison.OrdinalIgnoreCase))
+                {
+                    chosen = false;
+                    Console.WriteLine("*** Welcome to the Food Menu ***");
+                    csvcalls.FOOD(); //get to the csv file food
+                }
+                else if (choiceM.Equals("d", StringComparison.OrdinalIgnoreCase))
+                {
+                    chosen = false;
+                    Console.WriteLine("*** Welcome to the Drinks ***");
+                    csvcalls.DRINKS(); //get to the csv file drinks
+                }
+                else if (choiceM.Equals("111", StringComparison.OrdinalIgnoreCase))
+                {
+                    chosen = false;
+                    Console.WriteLine("*aanpassen*");
+                    csvcalls.CHANGEask();
+                }
+                else if (string.IsNullOrEmpty(choiceM))
+                {   //EMPTY INPUT
+                    Console.WriteLine("Not a valid input, please try again.");
+                }
+                else
+                {   //INVALID INPUT
+                    Console.WriteLine("Not a valid input, please try again.");
+                }
+            }
+            backgo();  
+        }
+
+        public static void backgo()
+        {
+            chosen = true;
+
+            Console.WriteLine("\nDo you want to go back?[yes/no]");
+            string goback = Console.ReadLine();
+
+            if (goback.Equals("yes", StringComparison.OrdinalIgnoreCase))
+            {
+                menu();
+            }
+            else if (goback.Equals("no", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.Write("\nWhat do you want to do?");
+                Console.WriteLine("(Enter \'help\' to view the options)");
+            }
+            else
+            {
+                Console.WriteLine("Not a valid input, please try again.");
+                backgo();
             }
         }
 
