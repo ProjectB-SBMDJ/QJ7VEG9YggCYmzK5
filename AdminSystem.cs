dc18b2266f253f544fa5c8adb188c05d37aec7b5
Dan00n <60699328+Dan00n@users.noreply.github.com>
2020-05-05 15:38:17 +0200

    nieuwe geupdate code

---
+++ AdminSystem.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace projectb {
+    public class AdminSystem {
+
+        private static string userDatabase = Directory.GetCurrentDirectory() + "/../../../json_files/users.json";
+
+        public static bool adminLoggedin;
+
+        private void GetUserDetails() {
+            var json = File.ReadAllText(userDatabase);
+            try {
+                var jObject = JObject.Parse(json);
+
+                if (jObject != null) {
+                    JArray usersArray = (JArray)jObject["users"];
+                    if (usersArray != null) {
+                        foreach (var item in usersArray) {
+                            Console.WriteLine("ID :" + item["userID"].ToString());
+                            Console.WriteLine("username :" + item["username"].ToString());
+                            Console.WriteLine("password :" + item["password"].ToString());
+                        }
+
+                    }
+                }
+            }
+            catch (Exception) {
+
+                throw;
+            }
+        }
+
+        public static void CheckUserDetails(string user, string pass) {
+            var json = File.ReadAllText(userDatabase);
+            try {
+                var jObject = JObject.Parse(json);
+
+                if (jObject != null) {
+                    JArray usersArray = (JArray)jObject["users"];
+                    if (usersArray != null) {
+                        foreach (var item in usersArray) {
+                            if (user == item["username"].ToString()) {
+                                if (pass == item["password"].ToString()) {
+                                    adminLoggedin = true;
+                                } else {
+                                    Console.WriteLine("Password is incorrect.");
+                                }
+                            } else {
+                                Console.WriteLine("User does not exist.");
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception) {
+
+                throw;
+            }
+        }
+
+        private void AddUser() {
+            Console.WriteLine("Enter Username : ");
+            var username = Console.ReadLine();
+            Console.WriteLine("\nEnter Password : ");
+            var password = Console.ReadLine();
+
+            var newUser = "{ 'username': " + username + ", 'password': '" + password + "'}";  
+            try  
+            {  
+                var json = File.ReadAllText(userDatabase);
+                var jsonObj = JObject.Parse(json);
+                var usersArray = jsonObj.GetValue("users") as JArray;
+                var newAdmin = JObject.Parse(newUser);
+                usersArray.Add(newAdmin);  
+  
+                jsonObj["users"] = usersArray;  
+                string newJsonResult = Newtonsoft.Json.JsonConvert.SerializeObject(jsonObj,
+                                       Newtonsoft.Json.Formatting.Indented);
+                File.WriteAllText(userDatabase, newJsonResult);  
+            }  
+            catch (Exception ex)  
+            {  
+                Console.WriteLine("Add Error : " + ex.Message.ToString());  
+            }  
+        }
+
+        private void UpdateUser() {
+            string json = File.ReadAllText(userDatabase);
+
+            try {
+                var jObject = JObject.Parse(json);
+                JArray usersArray = (JArray)jObject["users"];
+                Console.Write("Enter username : ");
+                var username = Convert.ToString(Console.ReadLine());
+
+                if (!string.IsNullOrEmpty(username)) {
+                    Console.Write("Enter new password : ");
+                    var password = Convert.ToString(Console.ReadLine());
+
+                    foreach (var user in usersArray.Where(obj => obj["username"].Value<string>() == username)) {
+                        user["password"] = !string.IsNullOrEmpty(password) ? password : "";
+                    }
+
+                    jObject["users"] = usersArray;
+                    string output = Newtonsoft.Json.JsonConvert.SerializeObject(jObject, Newtonsoft.Json.Formatting.Indented);
+                    File.WriteAllText(userDatabase, output);
+                }
+                else {
+                    Console.Write("Invalid Company ID, Try Again!");
+                    UpdateUser();
+                }
+            }
+            catch (Exception ex) {
+
+                Console.WriteLine("Update Error : " + ex.Message.ToString());
+            }
+        }
+
+        public AdminSystem() {
+       
+        }
+    }
+}
--- menu.cs
+++ menu.cs
@@ -4,7 +4,7 @@ using projectb;
 
 namespace testproject1
 {
-    
+
     public class Menu
     {
         static bool chosen = true;
@@ -50,13 +50,6 @@ namespace testproject1
                     Console.WriteLine("*** Welcome to the Drinks ***");
                     csvcalls.DRINKS(); //get to the csv file drinks
                 }
-                else if (choiceM.Equals("111", StringComparison.OrdinalIgnoreCase))
-                {
-                    //Dit is nog hier, maar dit moet naar het admin dingetje verplaats worden zodra hij klaar is
-                    chosen = false;
-                    Console.WriteLine("*aanpassen*");
-                    csvcalls.CHANGEask();
-                }
                 else if (string.IsNullOrEmpty(choiceM))
                 {   //EMPTY INPUT
                     Console.WriteLine("Not a valid input, please try again.");
@@ -66,21 +59,21 @@ namespace testproject1
                     Console.WriteLine("Not a valid input, please try again.");
                 }
             }
-            backgo();  
+            backgo();
         }
 
         public static void backgo()
         {
             chosen = true;
 
-            Console.WriteLine("\nDo you want to go back?[yes/no]");
+            Console.WriteLine("\nDo you want to go back to the main menu?[yes/no]");
             string goback = Console.ReadLine();
 
-            if (goback.Equals("yes", StringComparison.OrdinalIgnoreCase))
+            if (goback.Equals("no", StringComparison.OrdinalIgnoreCase))
             {
                 menu();
             }
-            else if (goback.Equals("no", StringComparison.OrdinalIgnoreCase))
+            else if (goback.Equals("yes", StringComparison.OrdinalIgnoreCase))
             {
                 Console.Write("\nWhat do you want to do?");
                 Console.WriteLine("(Enter \'help\' to view the options)");
