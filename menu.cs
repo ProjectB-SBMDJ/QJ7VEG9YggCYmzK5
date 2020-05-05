dae97803b24acb3475505ce2ecb0c491f835ee14
Dan00n <60699328+Dan00n@users.noreply.github.com>
2020-05-05 15:36:17 +0200

    code weggehaald van menu
    (zodat het werkt)

--- menu.cs
+++ menu.cs
@@ -4,7 +4,7 @@ using projectb;
 
 namespace testproject1
 {
-    
+
     public class Menu
     {
         static bool chosen = true;
@@ -13,9 +13,9 @@ namespace testproject1
             // day today
             var date = DateTime.Now;
             string a = date.ToString("dddd");
-            Console.WriteLine("-----------------------`    |\n-------------------------------");
+            Console.WriteLine("-------------------------------\n|     Welcome to the menu     |\n-------------------------------");
             Console.WriteLine(" [DO] - Daily offers \n [S] - Special offers\n [F] - Food\n [D] - Drink");
-          
+
             while (chosen)
             {
                 Console.Write("Choose an option: ");
@@ -50,13 +50,6 @@ namespace testproject1
                     Console.WriteLine("*** Welcome to the Drinks ***");
                     csvcalls.DRINKS(); //get to the csv file drinks
                 }
-                else if (choiceM.Equals("111", StringComparison.OrdinalIgnoreCase))
-                {
-                    //Dit is nog hier, maar dit moet naar het admin dingetje verplaats worden zodra hij klaar is
-                    chosen = false;
-                    Console.WriteLine("*aanpassen*");
-                    csvcalls.CHANGEask();
-                }
                 else if (string.IsNullOrEmpty(choiceM))
                 {   //EMPTY INPUT
                     Console.WriteLine("Not a valid input, please try again.");
@@ -66,21 +59,21 @@ namespace testproject1
                     Console.WriteLine("Not a valid input, please try again.");
                 }
             }
-            backgo();  
+            backgo();
         }
 
         public static void backgo()
         {
             chosen = true;
 
-            Console.WriteLine("\nDo you want to go back?[yes/no]");
+            Console.WriteLine("\nDo you want to go back to the main menu?[yes/no]");
             string goback = Console.ReadLine();
 
-            if (goback.Equals("yes", StringComparison.OrdinalIgnoreCase))
+            if (goback.Equals("no", StringComparison.OrdinalIgnoreCase))
             {
                 menu();
             }
-            else if (goback.Equals("no", StringComparison.OrdinalIgnoreCase))
+            else if (goback.Equals("yes", StringComparison.OrdinalIgnoreCase))
             {
                 Console.Write("\nWhat do you want to do?");
                 Console.WriteLine("(Enter \'help\' to view the options)");
@@ -90,52 +83,7 @@ namespace testproject1
                 Console.WriteLine("Not a valid input, please try again.");
                 backgo();
             }
-
-
-
-            Console.WriteLine("\n----Welcome to the Menu Page----");
-            Console.WriteLine("Enter \'help\' to see the options");
-      //     while (menuRunning)
-      //      {
-      //          readMenuInput();
-      //
-      //          switch (menuSelection.ToLower())
-      //         {
-      //              case "a":
-      //                  DailyOffer();
-      //                  Console.WriteLine("\nWhat would you like to do now? (enter \'help\' to see options)");
-      //                  break;
-      //             case "b":
-      //                 SpecialOffer();
-      //                 Console.WriteLine("\nWhat would you like to do now? (enter \'help\' to see options)");
-      //                  break;
-      //              case "c":
-      //                  Food();
-      //                  Console.WriteLine("\nWhat would you like to do now? (enter \'help\' to see options)");
-      //                  break;
-      //              case "d":
-      //                 Drink();
-      //                 Console.WriteLine("\nWhat would you like to do now? (enter \'help\' to see options)");
-      //                  break;
-      //              case "help":
-      //                  Menu();
-      //                  break;
-      //              case "e":
-      //                  Console.WriteLine("\n----Welcome Back To The Main Menu----");
-      //                  Console.WriteLine("Enter \'help\' to view the options!");
-      //                  menuRunning = false;
-      //                break;
-      //              case "":        //empty input
-      //              case null:      // is invalid
-      //                  Console.WriteLine("Empty input, please try again.");
-      //                  break;
-      //              default:
-      //                 Console.WriteLine("Not a valid input, please try again.");
-      //                  break;
-      //
-      //          }
-      //      }
         }
-    }
 
+    }
 }
